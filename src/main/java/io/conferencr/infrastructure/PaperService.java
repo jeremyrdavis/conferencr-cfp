14ff985ebda89426d1f4aba1f397a85339757827
Jeremy Davis <jeremyrdavis@mac.com>
2022-07-03 21:27:40 -0400

    passing 2 tests

--- src/main/java/io/conferencr/infrastructure/PaperService.java
+++ src/main/java/io/conferencr/infrastructure/PaperService.java
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import javax.transaction.Transactional;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -23,6 +24,7 @@ public class PaperService {
     @Inject
     SpeakerRepository speakerRepository;
 
+    @Transactional
     public Paper createFromPaperVO(final PaperVO paperVO) {
 
         Speaker speaker = speakerRepository.findByEmail(paperVO.speakerEmail());
